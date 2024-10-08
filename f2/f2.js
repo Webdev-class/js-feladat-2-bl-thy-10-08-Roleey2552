5778b90ebbea8a15daf788ff9af1b56daed614c3
Roleey2552 <szakaroli@gmail.com>
2024-10-08 14:23:48 +0200

    Update f2.js

--- f2/f2.js
+++ f2/f2.js
@@ -3,14 +3,30 @@ Készítsd el az előző feladat azon változatát ahol megadjuk az óraszámot 
 Csak az adott órára vonatkozó hőmérséklettel térjen vissza a függvény.
 */
 
-const locations = require('../locations.json');
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
+const APIAddress = 'https://archive-api.open-meteo.com/v1/archive?';
 
-const f2 = async (lat, long, timeStr, hour) => {
-    const str = APIAdress + `latitude=${lat}&longitude=${long}` +
-        `&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`;
-    const rArr = await fetch(str)
-        .then(data => data.json());
-    return rArr.hourly.temperature_2m.at(hour);
+async function f1(lat, long, timeStr) {
+    const date = timeStr.split('T')[0]; 
+    const hour = timeStr.split('T')[1]; 
+
+    
+    const url = `${APIAddress}latitude=${lat}&longitude=${long}&start_date=${date}&end_date=${date}&hourly=temperature_2m&timezone=auto`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Halózati hiba');
+        }
+        const data = await response.json();
+
+        
+        const hourIndex = parseInt(hour.split(':')[0]); 
+        const temperature = data.hourly.temperature_2m[hourIndex];
+
+        return Math.floor(temperature); 
+    } catch (error) {
+        console.error('Hiba történt fetcheléskor:', error);
+    }
 }
-module.exports = f2;
+
+module.exports = f1;
